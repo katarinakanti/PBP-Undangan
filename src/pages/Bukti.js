f0fab85f6c36a8c860eeffa2e56082c5cbbe6261
salsabilatuadaa <salsabilatuadaa7@gmail.com>
2023-09-04 10:01:42 +0700

    add styling bukti

--- src/pages/Bukti.js
+++ src/pages/Bukti.js
@@ -28,6 +28,8 @@ function App() {
         <p className = "App-Isi">GEDUNG FOREVER</p>
         <p className = "App-Isi">JALAN PIPOP KESAYANGAN BUNDA NO. 93 RT/RW 03/05,</p>
         <p className = "App-Isi">BULUSAN, SEMARANG, JAWA TENGAH 19051</p>
+        <br/>
+        <h1 className = "namaundangan">Kepada Bpk. & Ibu </h1>
 
       </section>
       
